72c1dd56ae9f7b4d91752eb7629e5b33e9e62bd2
Chaudhary-sonika <111914130+Chaudhary-sonika@users.noreply.github.com>
2023-08-12 21:59:33 +0530

    doc: add product management  inputs

--- src/pages/prodmanagement/ProdManagement.jsx
+++ src/pages/prodmanagement/ProdManagement.jsx
@@ -1,7 +1,20 @@
+import "../prodmanagement/ProdManagement.css";
 export const ProdManagement =()=>{
     return(
         <div>
-            <h1>Prod</h1>
+            <h3>Add New Product</h3>
+            <form className="form_tag">
+                <label>Department: <input/></label>
+                <label>Name: <input/></label>
+                <label>Description: <input/></label>
+                <label>Price: <input/></label>
+                <label>Stock: <input/></label>
+                <label>SKU: <input/></label>
+                <label>Supplier: <input/></label>
+                <label>Delivered: <input/></label>
+                <label>Image URL: <input/></label>
+                <button>Add Product</button>
+            </form>
         </div>
     )
 }
